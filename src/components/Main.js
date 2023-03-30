e9a66efb9a4b1e306efadd186a4ae370664647f4
MentalArea <113bennylee@gmail.com>
2023-03-30 10:21:52 -0400

    Updated Styles

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,25 @@
-import { Button, Container, Grid, Typography } from "@mui/material";
-import React from "react";
+import {
+  Button,
+  Container,
+  Grid,
+  Typography,
+  useMediaQuery
+} from "@mui/material";
+import React, { useEffect } from "react";
 import AppBar from "./AppBar.js";
 import Image from "next/image";
+import { useTheme } from "@mui/styles";
 
 export default function Main(props) {
+  const theme = useTheme();
+  const smallScreen = useMediaQuery(theme.breakpoints.down("md"));
+  const [windowWidth, setWindowWidth] = React.useState(0);
+  const [windowHeight, setWindowHeight] = React.useState(0);
+  useEffect(() => {
+    setWindowWidth(window.innerWidth);
+    setWindowHeight(window.innerHeight);
+  }, []);
+  console.log("theme", theme);
   return (
     <Container
       maxWidth={false}
@@ -16,10 +32,10 @@ export default function Main(props) {
       }}
     >
       <AppBar />
-      <Grid container flex={1}>
+      <Grid container flex={1} padding={10}>
         <Grid
           xs={12}
-          sm={6}
+          md={6}
           spacing={2}
           container
           item
@@ -27,12 +43,12 @@ export default function Main(props) {
           alignItems={"flex-end"}
           justifyContent="center"
         >
-          <Grid item container wrap="wrap" style={{ width: "50%" }}>
+          <Grid item container wrap="wrap">
             <Typography color="secondary" variant="h2">
               Control Your PC From Any Device
             </Typography>
           </Grid>
-          <Grid item container wrap="wrap" style={{ width: "50%" }}>
+          <Grid item container wrap="wrap">
             <Typography color="secondary" variant="h4">
               Your are only{" "}
               <p1
@@ -47,29 +63,58 @@ export default function Main(props) {
               away from what you need
             </Typography>
           </Grid>
-          <Grid item container wrap="wrap" style={{ width: "50%" }}>
+          <Grid item container wrap="wrap">
             <Typography color="secondary.dark" variant="body1">
               Project Hub is currently in development. We are working hard to
               bring you the best experience possible. Stay tuned for updates!
             </Typography>
           </Grid>
-          <Grid item container wrap="wrap" style={{ width: "50%" }}>
-            <Button
-              color="secondary"
-              variant="contained"
-              style={{ padding: 10 }}
-            >
-              Download Alpha Version
-            </Button>
+          <Grid item container wrap="wrap" spacing={1}>
+            <Grid item>
+              <Button
+                onClick={() => {
+                  window.open(
+                    "https://github.com/hysasuke/Project-Hub/releases/latest/download/x64.zip"
+                  );
+                }}
+                color="secondary"
+                variant="contained"
+                style={{ padding: 10 }}
+              >
+                Download for Windows(64-bit)
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button
+                onClick={() => {
+                  window.open(
+                    "https://github.com/hysasuke/Project-Hub/releases/latest/download/ia32.zip"
+                  );
+                }}
+                color="secondary"
+                variant="contained"
+                style={{ padding: 10 }}
+              >
+                Download for Windows(32-bit)
+              </Button>
+            </Grid>
           </Grid>
         </Grid>
-        <Grid xs={6} sm={6} alignItems={"center"} display={"flex"}>
-          <Image
-            src={"/Project-Website/main_hero.png"}
-            width={2649 / 2}
-            height={2000 / 2}
-          />
-        </Grid>
+        {!smallScreen && (
+          <Grid
+            xs={6}
+            md={6}
+            alignItems={"center"}
+            display={"flex"}
+            justifyContent="center"
+          >
+            <Image
+              src={"/Project-Website/main_hero.png"}
+              width={windowWidth / 3}
+              height={windowWidth / 3 / 1.5869}
+            />
+          </Grid>
+        )}
       </Grid>
     </Container>
   );
